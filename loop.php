b575f408cd77335fd076755fbe0adc21ca6140d0
khurshed-akand <khurshed.akand@gmail.com>
2022-09-10 16:17:40 +0600

    New File Add

--- loop.php
+++ loop.php
@@ -1,10 +1,10 @@
 <?php
 
 $even=" ";
-// $odd=" ";
-$i=15;
+$odd=" ";
+$i=1;
 
-while($i<=50)
+while($i<=100)
 {
 if($i%2==0)
 {
@@ -12,13 +12,13 @@ $even = $even . " " .$i ;
 }
 else
 {
-// $odd = $odd . " " .$i;
+$odd = $odd . " " .$i;
 }
 
 $i++;
 }
 
 echo "The Even No:" . $even . "<br>"; 
-// echo "The Odd No:" . $odd;
+echo "The Odd No:" . $odd;
 
 ?>
